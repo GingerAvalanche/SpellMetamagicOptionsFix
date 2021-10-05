e6e4249e1dd2a9b9a553d4e15abd88084b562661
GingerAvalanche <chodness@gmail.com>
2021-10-04 21:11:51 -0700

    Convert all formatting to spaces How did it end up mixed?

--- SpellMetamagicOptionsFix/ExtensionMethods.cs
+++ SpellMetamagicOptionsFix/ExtensionMethods.cs
@@ -5,50 +5,50 @@ using static SpellMetamagicOptionsFix.Helpers;
 
 namespace SpellMetamagicOptionsFix
 {
-	public static class ExtensionMethods
-	{
-		public static Helpers.FlatAbility FlattenAbility(this BlueprintAbility ability)
+    public static class ExtensionMethods
+    {
+        public static Helpers.FlatAbility FlattenAbility(this BlueprintAbility ability)
         {
             flatAbility = new();
-			flatAbility.Add(ability);
+            flatAbility.Add(ability);
 
-			foreach (FieldInfo field in ability.GetType().GetFields(AccessTools.all))
-			{
-				ParseObject(field.GetValue(ability));
-				if (flatAbility.AllMetamagicSet) { break; }
-			}
-
-			foreach (PropertyInfo property in ability.GetType().GetProperties(AccessTools.all))
-			{
-				ParseObject(property.GetValue(ability));
-				if (flatAbility.AllMetamagicSet) { break; }
-			}
-
-			return flatAbility;
-        }
-		public static void Flatten<T>(this T o)
-        {
-			if (o is null) { return; }
-			if (!flatAbility.Add(o))
+            foreach (FieldInfo field in ability.GetType().GetFields(AccessTools.all))
             {
-				return;
-			}
+                ParseObject(field.GetValue(ability));
+                if (flatAbility.AllMetamagicSet) { break; }
+            }
 
-			foreach (FieldInfo field in o.GetType().GetFields(AccessTools.all))
-			{
-				ParseObject(field.GetValue(o));
-				if (flatAbility.AllMetamagicSet) { return; }
-			}
+            foreach (PropertyInfo property in ability.GetType().GetProperties(AccessTools.all))
+            {
+                ParseObject(property.GetValue(ability));
+                if (flatAbility.AllMetamagicSet) { break; }
+            }
 
-			foreach (PropertyInfo property in o.GetType().GetProperties(AccessTools.all))
-			{
-				try
-				{
-					ParseObject(property.GetValue(o));
-				}
-				catch (TargetInvocationException) { }
-				if (flatAbility.AllMetamagicSet) { return; }
-			}
-		}
-	}
+            return flatAbility;
+        }
+        public static void Flatten<T>(this T o)
+        {
+            if (o is null) { return; }
+            if (!flatAbility.Add(o))
+            {
+                return;
+            }
+
+            foreach (FieldInfo field in o.GetType().GetFields(AccessTools.all))
+            {
+                ParseObject(field.GetValue(o));
+                if (flatAbility.AllMetamagicSet) { return; }
+            }
+
+            foreach (PropertyInfo property in o.GetType().GetProperties(AccessTools.all))
+            {
+                try
+                {
+                    ParseObject(property.GetValue(o));
+                }
+                catch (TargetInvocationException) { }
+                if (flatAbility.AllMetamagicSet) { return; }
+            }
+        }
+    }
 }
--- SpellMetamagicOptionsFix/Helpers.cs
+++ SpellMetamagicOptionsFix/Helpers.cs
@@ -16,145 +16,145 @@ using System.Linq;
 namespace SpellMetamagicOptionsFix
 {
     public static class Helpers
-	{
-		public static FlatAbility flatAbility;
-		public static List<BlueprintCharacterClassReference> classes;
-		public static SimpleBlueprint[] LocalBlueprintLib;
-		private static readonly Type[] ignoreTypes = new Type[] { typeof(SpellListComponent), typeof(SpellComponent), typeof(AddSpellImmunity) };
-		internal static void Load()
+    {
+        public static FlatAbility flatAbility;
+        public static List<BlueprintCharacterClassReference> classes;
+        public static SimpleBlueprint[] LocalBlueprintLib;
+        private static readonly Type[] ignoreTypes = new Type[] { typeof(SpellListComponent), typeof(SpellComponent), typeof(AddSpellImmunity) };
+        internal static void Load()
         {
-			Helpers.classes = (from c in ResourcesLibrary.GetRoot().Progression.m_CharacterClasses where c.Guid != "f5b8c63b141b2f44cbb8c2d7579c34f5" select c).ToList();
-			Dictionary<BlueprintGuid, BlueprintsCache.BlueprintCacheEntry> blueprints = (Dictionary<BlueprintGuid, BlueprintsCache.BlueprintCacheEntry>)AccessTools.Field(typeof(BlueprintsCache), "m_LoadedBlueprints").GetValue(ResourcesLibrary.BlueprintsCache);
-			BlueprintGuid[] keys = blueprints.Keys.ToArray();
-			LocalBlueprintLib = (from k in keys select ResourcesLibrary.TryGetBlueprint(k)).ToArray();
-		}
-		public static T[] GetBlueprints<T>() where T : SimpleBlueprint
-		{
-			return LocalBlueprintLib.OfType<T>().Distinct().ToArray();
-		}
-		public static void ParseObject(object o)
-		{
-			if (flatAbility.AllMetamagicSet) { return; }
-			if (o is null || ignoreTypes.Contains(o.GetType())) { return; }
-			if (o as BlueprintComponent is not null)
-			{
-				(o as BlueprintComponent).Flatten();
-			}
-			else if (o as BlueprintScriptableObject is not null)
-			{
-				(o as BlueprintScriptableObject).Flatten();
-			}
-			else if (o as GameAction is not null)
-			{
-				(o as GameAction).Flatten();
-			}
-			else if (o as ActionList is not null)
-			{
-				foreach (GameAction a in (o as ActionList).Actions)
-				{
-					a.Flatten();
-				}
-			}
-			else if (o as Array is not null)
-			{
-				Array a = o as Array;
-				for (var i = 0; i < a.Length; i++)
-				{
-					ParseObject(a.GetValue(i));
-				}
-			}
-			else if (o is ReferenceArrayProxy<BlueprintAbility, BlueprintAbilityReference> rap)
-			{
-				foreach (BlueprintAbility r in rap)
-				{
-					r.Flatten();
-				}
-			}
-		}
-		public class FlatAbility
+            Helpers.classes = (from c in ResourcesLibrary.GetRoot().Progression.m_CharacterClasses where c.Guid != "f5b8c63b141b2f44cbb8c2d7579c34f5" select c).ToList();
+            Dictionary<BlueprintGuid, BlueprintsCache.BlueprintCacheEntry> blueprints = (Dictionary<BlueprintGuid, BlueprintsCache.BlueprintCacheEntry>)AccessTools.Field(typeof(BlueprintsCache), "m_LoadedBlueprints").GetValue(ResourcesLibrary.BlueprintsCache);
+            BlueprintGuid[] keys = blueprints.Keys.ToArray();
+            LocalBlueprintLib = (from k in keys select ResourcesLibrary.TryGetBlueprint(k)).ToArray();
+        }
+        public static T[] GetBlueprints<T>() where T : SimpleBlueprint
         {
-			private readonly List<string> guids = new();
-			public Metamagic metamagic { get; private set; }
-			private bool EmpowerMaximize;
-			private bool Quicken;
-			private bool Extend;
-			private bool Reach;
-			private bool Bolstered;
-			public bool AllMetamagicSet { get; private set; }
-
-			public bool Add<T>(T o)
+            return LocalBlueprintLib.OfType<T>().Distinct().ToArray();
+        }
+        public static void ParseObject(object o)
+        {
+            if (flatAbility.AllMetamagicSet) { return; }
+            if (o is null || ignoreTypes.Contains(o.GetType())) { return; }
+            if (o as BlueprintComponent is not null)
             {
-				string hash = o.GetHashCode().ToString();
-				if (guids.Contains(hash))
+                (o as BlueprintComponent).Flatten();
+            }
+            else if (o as BlueprintScriptableObject is not null)
+            {
+                (o as BlueprintScriptableObject).Flatten();
+            }
+            else if (o as GameAction is not null)
+            {
+                (o as GameAction).Flatten();
+            }
+            else if (o as ActionList is not null)
+            {
+                foreach (GameAction a in (o as ActionList).Actions)
                 {
-					return false;
+                    a.Flatten();
+                }
+            }
+            else if (o as Array is not null)
+            {
+                Array a = o as Array;
+                for (var i = 0; i < a.Length; i++)
+                {
+                    ParseObject(a.GetValue(i));
+                }
+            }
+            else if (o is ReferenceArrayProxy<BlueprintAbility, BlueprintAbilityReference> rap)
+            {
+                foreach (BlueprintAbility r in rap)
+                {
+                    r.Flatten();
+                }
+            }
+        }
+        public class FlatAbility
+        {
+            private readonly List<string> guids = new();
+            public Metamagic metamagic { get; private set; }
+            private bool EmpowerMaximize;
+            private bool Quicken;
+            private bool Extend;
+            private bool Reach;
+            private bool Bolstered;
+            public bool AllMetamagicSet { get; private set; }
+
+            public bool Add<T>(T o)
+            {
+                string hash = o.GetHashCode().ToString();
+                if (guids.Contains(hash))
+                {
+                    return false;
                 }
 
-				if (!EmpowerMaximize) { CheckEmpowerMaximizeBolstered(o); }
-				if (!Extend) { CheckExtend(o); }
+                if (!EmpowerMaximize) { CheckEmpowerMaximizeBolstered(o); }
+                if (!Extend) { CheckExtend(o); }
 
-				if (o is BlueprintAbility spell)
-				{
-					if (!Quicken) { CheckQuicken(spell); }
-					if (!Reach) { CheckReach(spell); }
-				}
+                if (o is BlueprintAbility spell)
+                {
+                    if (!Quicken) { CheckQuicken(spell); }
+                    if (!Reach) { CheckReach(spell); }
+                }
 
-				guids.Add(hash);
-				return true;
+                guids.Add(hash);
+                return true;
             }
 
-			private void CheckEmpowerMaximizeBolstered(object o)
-			{
-				bool UsesDice = o.GetType().GetFields(AccessTools.all)
-					.Where(f => f.FieldType == typeof(ContextDiceValue) && (ContextDiceValue)f.GetValue(o) is not null)
-					.Select(f => (ContextDiceValue)f.GetValue(o))
-					.Any(v => v.DiceType != DiceType.Zero);
+            private void CheckEmpowerMaximizeBolstered(object o)
+            {
+                bool UsesDice = o.GetType().GetFields(AccessTools.all)
+                    .Where(f => f.FieldType == typeof(ContextDiceValue) && (ContextDiceValue)f.GetValue(o) is not null)
+                    .Select(f => (ContextDiceValue)f.GetValue(o))
+                    .Any(v => v.DiceType != DiceType.Zero);
 
                 if (UsesDice)
-				{
-					if (!EmpowerMaximize)
-					{
-						EmpowerMaximize = true;
-						metamagic |= Metamagic.Empower | Metamagic.Maximize;
-						AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
-					}
-					if (!Bolstered && o.GetType() == typeof(ContextActionDealDamage))
-					{
-						Bolstered = ((ContextActionDealDamage)o).IsSimpleDamage;
-						if (Bolstered)
-						{
-							metamagic |= Metamagic.Bolstered;
-							AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
-						}
-					}
-				}
+                {
+                    if (!EmpowerMaximize)
+                    {
+                        EmpowerMaximize = true;
+                        metamagic |= Metamagic.Empower | Metamagic.Maximize;
+                        AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
+                    }
+                    if (!Bolstered && o.GetType() == typeof(ContextActionDealDamage))
+                    {
+                        Bolstered = ((ContextActionDealDamage)o).IsSimpleDamage;
+                        if (Bolstered)
+                        {
+                            metamagic |= Metamagic.Bolstered;
+                            AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
+                        }
+                    }
+                }
             }
-			private void CheckQuicken(BlueprintAbility spell)
-			{
-				Quicken = spell.IsStandardAction || spell.IsFullRoundAction;
-				if (Quicken) { metamagic |= Metamagic.Quicken; }
-            }
-			private void CheckExtend(object o)
+            private void CheckQuicken(BlueprintAbility spell)
             {
-				Extend = o.GetType().GetFields(AccessTools.all)
-					.Where(f => f.FieldType == typeof(ContextDurationValue) && (ContextDurationValue)f.GetValue(o) is not null)
-					.Select(f => (ContextDurationValue)f.GetValue(o))
-					.Any(v => v.IsExtendable);
-				if (Extend)
-				{
-					metamagic |= Metamagic.Extend;
-					AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
-				}
+                Quicken = spell.IsStandardAction || spell.IsFullRoundAction;
+                if (Quicken) { metamagic |= Metamagic.Quicken; }
             }
-			private void CheckReach(BlueprintAbility spell)
+            private void CheckExtend(object o)
             {
-				Reach = (int)spell.Range < 3;
-				if (Reach)
-				{
-					metamagic |= Metamagic.Reach;
-					AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
-				}
+                Extend = o.GetType().GetFields(AccessTools.all)
+                    .Where(f => f.FieldType == typeof(ContextDurationValue) && (ContextDurationValue)f.GetValue(o) is not null)
+                    .Select(f => (ContextDurationValue)f.GetValue(o))
+                    .Any(v => v.IsExtendable);
+                if (Extend)
+                {
+                    metamagic |= Metamagic.Extend;
+                    AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
+                }
             }
-		}
-	}
+            private void CheckReach(BlueprintAbility spell)
+            {
+                Reach = (int)spell.Range < 3;
+                if (Reach)
+                {
+                    metamagic |= Metamagic.Reach;
+                    AllMetamagicSet = EmpowerMaximize && Quicken && Extend && Reach && Bolstered;
+                }
+            }
+        }
+    }
 }
